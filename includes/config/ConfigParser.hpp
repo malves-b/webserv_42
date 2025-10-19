9a3164eb352a6f961e6b96d4b9848d1afd1c7840
Mariana <mariana.obm@gmail.com>
2025-10-19 14:25:44 +0100

    Working on ConfigParser

--- includes/config/ConfigParser.hpp
+++ includes/config/ConfigParser.hpp
@@ -3,16 +3,24 @@
 
 #include <Config.hpp>
 #include <string>
+#include <iostream>
 
 class ConfigParser
 {
 	private:
+		static std::istringstream	cleanConfigFile(std::ifstream& file);
+		static void					parseServerBlock(std::istream& in, Config& config);
+		static void					parseLocationBlock(std::istream& in, ServerConfig& server);
+		static std::string			nextToken(std::istream& in);
+		static void					expect(std::istream& in, std::string const& expected);
+
 		ConfigParser(std::string file);
 		ConfigParser(ConfigParser const& src);
-		ConfigParser&		operator=(ConfigParser const& rhs);
+		ConfigParser&				operator=(ConfigParser const& rhs);
 		~ConfigParser(void);
 	public:
-		static Config		parseFile(std::string const& configFile); //populates the Config::_servers vector //dont allow port duplicates
+		static Config				parseFile(std::string const& configFile); //populates the Config::_servers vector //dont allow port duplicates
+
 };
 
 #endif //CONFIGPARSER_H
--- srcs/config/ConfigParser.cpp
+++ srcs/config/ConfigParser.cpp
@@ -1,3 +1,76 @@
 #include <config/ConfigParser.hpp>
+#include <fstream> 
+#include <sstream>
 
-//ServerConfig	ConfigParser::parseFile(std::string const& configFile) {}
+
+std::string	ConfigParser::nextToken(std::istream& in)
+{
+	std::string	token;
+	in >> token;
+	if (!in)
+		throw std::runtime_error("Unexpected end of file");
+	return (token);
+}
+
+void	ConfigParser::expect(std::istream& in, std::string const& expected)
+{
+	std::string	token = nextToken(in);
+	if (token != expected)
+		throw std::runtime_error("Expected '" + expected + "', got '" + token + "'");
+}
+
+void	ConfigParser::parseServerBlock(std::istream& in, Config& config)
+{
+	expect(in, "{");
+
+	ServerConfig	server;
+	std::string		token;
+
+	while (in >> token)
+	{
+		if (token == "}")
+			break ;
+		else if (token == "listen")
+	}
+
+}
+
+std::istringstream	ConfigParser::cleanConfigFile(std::ifstream& file)
+{
+	std::stringstream	clean;
+	std::string			line;
+
+	while (std::getline(file, line))
+	{
+		std::string::size_type	commentPos = line.find('#');
+		if (commentPos != std::string::npos)
+			line.erase(commentPos); //erase comments
+		if (!line.empty())
+			clean << line << '\n';
+	}
+	file.close();
+	std::istringstream res(clean.str());
+	return (res);
+}
+
+
+Config	ConfigParser::parseFile(std::string const& configFile)
+{
+	std::ifstream	rawFile(configFile.c_str());
+	if (!rawFile.is_open())
+		throw std::runtime_error("Filed to open config file: " + configFile);
+
+	std::istringstream file = cleanConfigFile(rawFile);
+
+	Config		config;
+	std::string	token;
+
+	while (file >> token) //automatically skips all normal whitespace between tokens (spaces, tabs, newlines)
+	{
+		if (token == "server")
+			parseServerBlock(file, config);
+		else
+			throw std::runtime_error("Unknown directive: " + token);
+	}
+	return (config);
+}
