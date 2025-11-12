f9874b2ea559bd496e45c94cd46d0a121646bff8
Jose Junior <jose_evilasio@outlook.com>
2025-11-12 13:07:33 +0000

    updated

--- webservinho_app/cgi-bin/sleep.py
+++ webservinho_app/cgi-bin/sleep.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import time
 print("Content-Type: text/plain\r\n\r\n")
-print("Sleeping for 100 seconds...")
-time.sleep(100)
+print("Sleeping for 35 seconds...")
+time.sleep(35)
 print("Done!")
